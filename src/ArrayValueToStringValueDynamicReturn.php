24e1e7b836973f321d1ae56d34eebb00bbf849bf
Bronisław Białek <after89@gmail.com>
2019-01-12 15:38:53 +0100

    Add toAssocValue and toStringValue support

---
+++ src/ArrayValueToStringValueDynamicReturn.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace GW\PHPStan\GwValues;
+
+use GW\Value\ArrayValue;
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\Type;
+
+final class ArrayValueToStringValueDynamicReturn implements DynamicMethodReturnTypeExtension
+{
+    public function getClass(): string
+    {
+        return ArrayValue::class;
+    }
+
+    public function isMethodSupported(MethodReflection $methodReflection): bool
+    {
+        return $methodReflection->getName() === 'toStringValue';
+    }
+
+    public function getTypeFromMethodCall(
+        MethodReflection $methodReflection,
+        MethodCall $methodCall,
+        Scope $scope
+    ): Type {
+        return new StringValueType();
+    }
+}
